1dfa6490ab673e47af218867108113eded699851
Jon Kimmel <jonkimmel16@gmail.com>
2020-01-08 13:58:20 -0700

    remove articles add ie depreciation

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ function teamDisplay() {
     $('#upperAbout').append(`
         <img src="Images/stars.jpg" alt="Looking at stars from narrow canyon." class="aboutStarsImage">
         <h1>Your Valuable Team at<br>
-            Jim Kimmel & Associates</h1>
+            James F. Kimmel & Associates</h1>
     `);
 
     $('#lowerAbout').append(`
